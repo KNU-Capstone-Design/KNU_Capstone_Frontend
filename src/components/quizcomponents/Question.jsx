18a7401d09e283915b7eb7aa69c7478fc90353d0
DongwooAn00 <hijason3596@gmail.com>
2025-05-13 11:35:50 +0900

    카테고리 문구 2차 수정

--- src/components/quizcomponents/Question.jsx
+++ src/components/quizcomponents/Question.jsx
@@ -4,7 +4,7 @@ import styles from './Question.module.css';
 function Question({ data }) {
   return (
     <div className={styles.questionBox}>
-      <p className={styles.category}>[{data.category}에 관련된 질문이에요!]</p>
+      <p className={styles.category}>{data.category}에 관련된 질문이에요!👏</p>
       <h2 className={styles.text}>{data.question}</h2>
     </div>
   );
