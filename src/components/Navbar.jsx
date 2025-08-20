52dd0e6c549c898033f3246d3ba6e87e135709b9
kimsunho2000 <sunhokim28@gmail.com>
2025-08-20 16:47:39 +0900

    네비게이션 로고에 버튼기능 추가 및 네비게이션 바 크기 조정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,24 @@ import styles from './Navbar.module.css';
 import logoImage from '../assets/logo.png';
 import SubscribeButton from './SubscribeButton';
 
+const baseURL = import.meta.env.VITE_API_BASEURL;
+
 function Navbar({ showSubscribe = true, onSubscribeClick}) {
+    const handleLogoClick = () => {
+        window.location.href = baseURL;
+    };
+
     return (
       <nav className={styles.navbar}>
         <div className={styles.navContent}>
           <div className={styles.logoContainer}>
-            <img src={logoImage} alt="로고" className={styles.logoImage} />
+            <button
+              className={styles.logoButton}
+              onClick={handleLogoClick}
+              aria-label="홈으로 이동"
+            >
+              <img src={logoImage} alt="로고" className={styles.logoImage} />
+            </button>
           </div>
   
           {showSubscribe && (
